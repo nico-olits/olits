f9cbcaed58df4f13f22d7c92756c57b1d93276d6
rtrolliard <r.trolliard@lyon.ort.asso.fr>
2015-12-10 14:42:21 +0100

    cpp

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,14 @@
+// ConsoleApplication1.cpp : définit le point d'entrée pour l'application console.
+//
+
+#include "stdafx.h"
+#include "iostream"
+
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout << "coucou" << endl;
+	return 0;
+}
+
